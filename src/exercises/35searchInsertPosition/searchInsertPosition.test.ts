2c90616b0ff3b312125428b03f44b7e26e39d567
LUIS GERARDO AGUILAR GARCIA <eagledluis.game@gmail.com>
2024-09-20 23:23:35 -0500

    feat: search insert recursive and iterable

--- src/exercises/35searchInsertPosition/searchInsertPosition.test.ts
+++ src/exercises/35searchInsertPosition/searchInsertPosition.test.ts
@@ -1,17 +1,33 @@
-import { searchInsert } from './searchInsertPosition';
+import {
+  recursiveSearchInsert,
+  iterableSearchInsert,
+} from './searchInsertPosition';
 describe('35. Search Insert Position', () => {
   // Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
   test('#1 it should return the current index or the index where it would be if it were inserted in order', () => {
-    expect(searchInsert([1, 3, 5, 6], 5)).toEqual(2);
+    expect(recursiveSearchInsert([1, 3, 5, 6], 5)).toEqual(2);
   });
-  test('#1 it should return the current index or the index where it would be if it were inserted in order', () => {
-    expect(searchInsert([1, 3, 5, 6], 2)).toEqual(1);
+  test('#2 it should return the current index or the index where it would be if it were inserted in order', () => {
+    expect(recursiveSearchInsert([1, 3, 5, 6], 2)).toEqual(1);
   });
-  test('#1 it should return the current index or the index where it would be if it were inserted in order', () => {
-    expect(searchInsert([1, 3, 5, 6], 7)).toEqual(4);
+  test('#3 it should return the current index or the index where it would be if it were inserted in order', () => {
+    expect(recursiveSearchInsert([1, 3, 5, 6], 7)).toEqual(4);
   });
 
+  test('#4 it should return the current index or the index where it would be if it were inserted in order', () => {
+    expect(recursiveSearchInsert([3, 4, 5, 6], 1)).toEqual(0);
+  });
   test('#1 it should return the current index or the index where it would be if it were inserted in order', () => {
-    expect(searchInsert([3, 4, 5, 6], 1)).toEqual(0);
+    expect(iterableSearchInsert([1, 3, 5, 6], 5)).toEqual(2);
+  });
+  test('#2 it should return the current index or the index where it would be if it were inserted in order', () => {
+    expect(iterableSearchInsert([1, 3, 5, 6], 2)).toEqual(1);
+  });
+  test('#3 it should return the current index or the index where it would be if it were inserted in order', () => {
+    expect(iterableSearchInsert([1, 3, 5, 6], 7)).toEqual(4);
+  });
+
+  test('#4 it should return the current index or the index where it would be if it were inserted in order', () => {
+    expect(iterableSearchInsert([3, 4, 5, 6], 1)).toEqual(0);
   });
 });
--- src/exercises/35searchInsertPosition/searchInsertPosition.ts
+++ src/exercises/35searchInsertPosition/searchInsertPosition.ts
@@ -1,10 +1,36 @@
-export function searchInsert(nums: number[], target: number): number {
-  let index = 0;
-  for (const num of nums) {
-    if (num >= target) {
-      return index;
+export function recursiveSearchInsert(nums: number[], target: number): number {
+  function search(
+    nums: number[],
+    target: number,
+    startPointer: number,
+    endPointer: number
+  ): number {
+    if (startPointer === endPointer) {
+      return target > nums[startPointer] ? ++startPointer : startPointer;
     }
-    index++;
+    const middlePointer = Math.round((endPointer - startPointer) / 2);
+    if (target === nums[middlePointer]) {
+      return middlePointer;
+    }
+    if (target > nums[middlePointer]) {
+      return search(nums, target, middlePointer + 1, endPointer);
+    } else {
+      return search(nums, target, startPointer, middlePointer - 1);
+    }
   }
-  return index;
+  return search(nums, target, 0, nums.length - 1);
+}
+
+export function iterableSearchInsert(nums: number[], target: number): number {
+  let startPointer = 0;
+  let endPointer = nums.length - 1;
+  while (startPointer !== endPointer) {
+    const middlePointer = Math.round((startPointer + endPointer) / 2);
+    if (target > nums[middlePointer]) {
+      startPointer = middlePointer;
+    } else {
+      endPointer = middlePointer - 1;
+    }
+  }
+  return target > nums[startPointer] ? ++startPointer : startPointer;
 }
