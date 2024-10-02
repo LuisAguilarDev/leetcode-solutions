c546ba9e1b55de066acd071e0480abb41c0fff82
LUIS GERARDO AGUILAR GARCIA <eagledluis.game@gmail.com>
2024-10-01 23:10:59 -0500

    feat: all tree traversal techniques

--- src/exercises/94binaryTreeInorderTraversal/binaryTreeInorderTraversal.test.ts
+++ src/exercises/94binaryTreeInorderTraversal/binaryTreeInorderTraversal.test.ts
@@ -1,22 +1,25 @@
 import {
   arrayToTreeNode,
   buildTreeFromArray,
-  inorderTraversal,
+  inorderTraversalIterative,
   inorderTraversalRecursive,
   levelOrderTraversalIterative,
+  levelOrderTraversalRecursive,
   postorderTraversalIterative,
+  postorderTraversalRecursive,
+  preorderTraversalIterative,
   preorderTraversalRecursive,
 } from './binaryTreeInorderTraversal';
 
 describe('94. Binary Tree Inorder Traversal', () => {
   test("#1 Given the root of a binary tree, return the inorder traversal of its nodes' values.", () => {
     expect(
-      inorderTraversal(arrayToTreeNode([1, null, 2, null, null, 3]))
+      inorderTraversalIterative(arrayToTreeNode([1, null, 2, null, null, 3]))
     ).toEqual([1, 3, 2]);
   });
   test("#2 Given the root of a binary tree, return the inorder traversal of its nodes' values.", () => {
     expect(
-      inorderTraversal(
+      inorderTraversalIterative(
         arrayToTreeNode([
           1,
           2,
@@ -37,10 +40,10 @@ describe('94. Binary Tree Inorder Traversal', () => {
     ).toEqual([4, 2, 6, 5, 7, 1, 3, 9, 8]);
   });
   test("#3 Given the root of a binary tree, return the inorder traversal of its nodes' values.", () => {
-    expect(inorderTraversal(arrayToTreeNode(null))).toEqual([]);
+    expect(inorderTraversalIterative(arrayToTreeNode(null))).toEqual([]);
   });
   test("#4 Given the root of a binary tree, return the inorder traversal of its nodes' values.", () => {
-    expect(inorderTraversal(arrayToTreeNode([1]))).toEqual([1]);
+    expect(inorderTraversalIterative(arrayToTreeNode([1]))).toEqual([1]);
   });
   test('#5 testing the leet code array builder', () => {
     const arr = [1, 2, 3, 4, 5, null, 8, null, null, 6, 7, 9];
@@ -83,17 +86,62 @@ describe('94. Binary Tree Inorder Traversal', () => {
   });
   test("#10 Given the root of a binary tree, return the preorder traversal of its nodes' values.", () => {
     expect(
+      preorderTraversalIterative(arrayToTreeNode([1, 2, 3, 4, 5, 6, 7]))
+    ).toEqual([1, 2, 4, 5, 3, 6, 7]);
+  });
+  test("#11 Given the root of a binary tree, return the preorder traversal of its nodes' values.", () => {
+    expect(
+      preorderTraversalRecursive(arrayToTreeNode([1, 2, 3, null, null, 6, 7]))
+    ).toEqual([1, 2, 3, 6, 7]);
+  });
+  test("#12 Given the root of a binary tree, return the preorder traversal of its nodes' values.", () => {
+    expect(
       preorderTraversalRecursive(arrayToTreeNode([1, 2, 3, 4, 5, 6, 7]))
     ).toEqual([1, 2, 4, 5, 3, 6, 7]);
   });
-  xtest("#11 Given the root of a binary tree, return the postorder traversal of its nodes' values.", () => {
+  test("#13 Given the root of a binary tree, return the preorder traversal of its nodes' values.", () => {
+    expect(
+      preorderTraversalRecursive(arrayToTreeNode([1, 2, 3, null, null, 6, 7]))
+    ).toEqual([1, 2, 3, 6, 7]);
+  });
+  test("#14 Given the root of a binary tree, return the postorder traversal of its nodes' values.", () => {
     expect(
       postorderTraversalIterative(arrayToTreeNode([1, 2, 3, 4, 5, 6, 7]))
     ).toEqual([4, 5, 2, 6, 7, 3, 1]);
   });
-  xtest("#12 Given the root of a binary tree, return the level order traversal of its nodes' values.", () => {
+  test("#15 Given the root of a binary tree, return the postorder traversal of its nodes' values.", () => {
+    expect(
+      postorderTraversalIterative(arrayToTreeNode([1, 2, 3, null, null, 6, 7]))
+    ).toEqual([2, 6, 7, 3, 1]);
+  });
+  test("#16 Given the root of a binary tree, return the postorder traversal of its nodes' values.", () => {
+    expect(
+      postorderTraversalRecursive(arrayToTreeNode([1, 2, 3, 4, 5, 6, 7]))
+    ).toEqual([4, 5, 2, 6, 7, 3, 1]);
+  });
+  test("#17 Given the root of a binary tree, return the postorder traversal of its nodes' values.", () => {
+    expect(
+      postorderTraversalRecursive(arrayToTreeNode([1, 2, 3, null, null, 6, 7]))
+    ).toEqual([2, 6, 7, 3, 1]);
+  });
+  test("#18 Given the root of a binary tree, return the level order traversal of its nodes' values.", () => {
     expect(
       levelOrderTraversalIterative(arrayToTreeNode([1, 2, 3, 4, 5, 6, 7]))
+    ).toEqual([1, 2, 3, 4, 5, 6, 7]);
+  });
+  test("#19 Given the root of a binary tree, return the level order traversal of its nodes' values.", () => {
+    expect(
+      levelOrderTraversalIterative(arrayToTreeNode([1, 2, 3, 4, 5, 6, 7]))
+    ).toEqual([1, 2, 3, 4, 5, 6, 7]);
+  });
+  test("#20 Given the root of a binary tree, return the level order traversal of its nodes' values.", () => {
+    expect(
+      levelOrderTraversalRecursive(arrayToTreeNode([1, 2, 3, 4, 5, 6, 7]))
+    ).toEqual([1, 2, 3, 4, 5, 6, 7]);
+  });
+  test("#21 Given the root of a binary tree, return the level order traversal of its nodes' values.", () => {
+    expect(
+      levelOrderTraversalRecursive(arrayToTreeNode([1, 2, 3, 4, 5, 6, 7]))
     ).toEqual([1, 2, 3, 4, 5, 6, 7]);
   });
 });
--- src/exercises/94binaryTreeInorderTraversal/binaryTreeInorderTraversal.ts
+++ src/exercises/94binaryTreeInorderTraversal/binaryTreeInorderTraversal.ts
@@ -1,10 +1,11 @@
 import { TreeNode } from '../../dataTypes/binaryTree/binaryTree';
-import { dynamicStack } from '../../dataTypes/stack/stackFixed';
+import { DynamicStack } from '../../dataTypes/stack/stackFixed';
+import { DinamycQueue } from '../../dataTypes/queue/queue';
 
-export function inorderTraversal(root: TreeNode | null): number[] {
-  const stack = new dynamicStack<TreeNode>();
+export function inorderTraversalIterative(root: TreeNode | null): number[] {
+  const response: number[] = [];
+  const stack = new DynamicStack<TreeNode>();
   let current = root;
-  let response = [];
   while (current || !stack.isEmpty()) {
     while (current) {
       stack.push(current);
@@ -12,74 +13,126 @@ export function inorderTraversal(root: TreeNode | null): number[] {
     }
     current = stack.pop();
     response.push(current!.val);
-    current = current!.right;
+    current = current?.right ?? null;
   }
   return response;
 }
 
 export function inorderTraversalRecursive(node: TreeNode | null): number[] {
-  if (!node) return [];
-  function recursiveFunc(node: TreeNode | null): any {
+  const result: number[] = [];
+  function recursiveFunc(node: TreeNode | null) {
     if (!node) return [];
-    return [
-      ...recursiveFunc(node?.left),
-      node.val,
-      ...recursiveFunc(node?.right),
-    ];
+    recursiveFunc(node.left);
+    result.push(node.val);
+    recursiveFunc(node.right);
   }
-  return recursiveFunc(node);
+  recursiveFunc(node);
+  return result;
+}
+
+export function preorderTraversalIterative(node: TreeNode | null): number[] {
+  if (!node) return [];
+  const response: number[] = [];
+  const stack = new DynamicStack<TreeNode>();
+  stack.push(node);
+  while (!stack.isEmpty()) {
+    let current = stack.pop();
+    response.push(current!.val);
+    if (current?.right) {
+      stack.push(current.right);
+    }
+    if (current?.left) {
+      stack.push(current.left);
+    }
+  }
+  return response;
 }
 
 export function preorderTraversalRecursive(node: TreeNode | null): number[] {
   if (!node) return [];
-  function recursiveFunc(node: TreeNode | null): any {
-    if (!node) return [];
-    return [
-      node.val,
-      ...recursiveFunc(node?.left),
-      ...recursiveFunc(node?.right),
-    ];
+  const response: number[] = [];
+  function recursiveFunction(node: TreeNode | null): void {
+    if (!node) return;
+    response.push(node.val);
+    recursiveFunction(node.left);
+    recursiveFunction(node.right);
   }
-  return recursiveFunc(node);
+  recursiveFunction(node);
+  return response;
 }
 
 export function postorderTraversalIterative(node: TreeNode | null): number[] {
-  //TODO:
   if (!node) return [];
-  function recursiveFunc(node: TreeNode | null): any {
-    if (!node) return [];
-    if (!node.left && !node.right) {
-      return [node.val, ...recursiveFunc(node)];
+  const response: number[] = [];
+  const stack = new DynamicStack<[TreeNode, boolean]>(); //[Node - firstVisit]
+  stack.push([node, true]);
+  while (!stack.isEmpty()) {
+    const [current, firstVisit] = stack.pop()!;
+    if (firstVisit) {
+      stack.push([current, false]);
+      if (current.right) {
+        stack.push([current.right, true]);
+      }
+      if (current.left) {
+        stack.push([current.left, true]);
+      }
+    } else {
+      response.push(current.val);
     }
-    return [
-      ...recursiveFunc(node?.left || null),
-      ...recursiveFunc(node?.right || null),
-    ];
   }
-  return [...recursiveFunc(node)];
+  return response;
+}
+
+export function postorderTraversalRecursive(node: TreeNode | null): number[] {
+  if (!node) return [];
+  const response: number[] = [];
+  function recursiveFunction(node: TreeNode | null): void {
+    if (!node) return;
+    recursiveFunction(node.left);
+    recursiveFunction(node.right);
+    response.push(node.val);
+  }
+  recursiveFunction(node);
+  return response;
 }
 
 export function levelOrderTraversalIterative(node: TreeNode | null): number[] {
-  //TODO:
   if (!node) return [];
-  function recursiveFunc(
-    node: TreeNode | null,
-    direction: 'left' | 'rigth'
-  ): any {
-    if (!node) return [];
-    return [
-      node.val,
-      ...recursiveFunc(node?.left || null, 'left'),
-      ...recursiveFunc(node?.right || null, 'rigth'),
-    ];
+  const response: number[] = [];
+  const queue = new DinamycQueue<TreeNode>();
+  queue.enqueue(node);
+  while (!queue.isEmpty()) {
+    const current = queue.dequeue()!;
+    response.push(current.val);
+    if (current.left) {
+      queue.enqueue(current.left);
+    }
+    if (current.right) {
+      queue.enqueue(current.right);
+    }
   }
-  return [
-    node.val,
-    ...recursiveFunc(node.left, 'left'),
-    ...recursiveFunc(node.right, 'rigth'),
-  ];
+  return response;
 }
-
+export function levelOrderTraversalRecursive(node: TreeNode | null): number[] {
+  if (!node) return [];
+  const response: number[] = [];
+  function recursiveFunction(nodes: TreeNode[]): void {
+    if (!nodes.length) return;
+    const nextNodes: TreeNode[] = [];
+    nodes.forEach((node) => {
+      response.push(node.val);
+      if (node.left) {
+        nextNodes.push(node.left);
+      }
+      if (node.right) {
+        nextNodes.push(node.right);
+      }
+    });
+    recursiveFunction(nextNodes);
+  }
+  recursiveFunction([node]);
+  return response;
+}
 export function arrayToTreeNode(data: any[] | null): TreeNode | null {
   if (!data) return null;
   if (!data.length) return null;
@@ -94,7 +147,6 @@ export function arrayToTreeNode(data: any[] | null): TreeNode | null {
 }
 
 export function buildTreeFromArray(arr: Array<number | null> | null) {
-  //??[1, 2, 3, 4, 5, null, 8, null, null, 6, 7, null, null, 9]
   if (!arr) return null;
   if (arr.length === 0) return null;
   const root = new TreeNode(arr[0]!);
@@ -117,3 +169,7 @@ export function buildTreeFromArray(arr: Array<number | null> | null) {
 
   return root;
 }
+
+//arbol con n hijos
+//node.val Node.childre:node[] = []
+//bfs && dfs(pre-order) //iterativo y recursivo
