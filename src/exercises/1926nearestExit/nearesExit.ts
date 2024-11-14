b432c61214db30a9808c1067ee407e741b8291c7
LUIS GERARDO AGUILAR GARCIA <eagledluis.game@gmail.com>
2024-11-14 15:33:50 -0500

    feat: nearestExitDfs

--- src/exercises/1926nearestExit/nearesExit.ts
+++ src/exercises/1926nearestExit/nearesExit.ts
@@ -35,3 +35,40 @@ export function nearestExit(
   }
   return -1;
 }
+
+export function nearestExitDfs(
+  maze: string[][],
+  entrance: number[],
+): number[] | number {
+  // prettier-ignore
+  const directions = [[1, 0],[-1, 0],[0, 1],[0, -1]];
+  const [IROWS, ICOLS] = [maze.length - 1, maze[0].length - 1];
+  console.log(IROWS, ICOLS);
+  const stack = [[...entrance, 0]];
+  const distances = new Map();
+  distances.set(entrance.toString(), 0);
+  let minDistance = Infinity;
+  while (stack.length) {
+    const [r, c, n] = stack.pop()!;
+    const isStart = n === 0;
+    const isExitRow = r === 0 || r === IROWS;
+    const isExitCol = c === 0 || c === ICOLS;
+    if (!isStart && (isExitRow || isExitCol) && n < minDistance) {
+      minDistance = n;
+      continue;
+    }
+    for (const [dr, dc] of directions) {
+      const [nr, nc] = [r + dr, c + dc];
+      const position = maze[nr]?.[nc];
+      const nextPositionKey = [nr, nc].toString();
+      const currentDistance = distances.get(nextPositionKey);
+      if (!position) continue;
+      if (position === '+') continue;
+      if (currentDistance === 0) continue;
+      if (currentDistance && currentDistance <= n + 1) continue;
+      distances.set(nextPositionKey, n + 1);
+      stack.push([nr, nc, n + 1]);
+    }
+  }
+  return minDistance === Infinity ? -1 : minDistance;
+}
